9eaec4d91b4b89f028da1c9abcf0fe1735c64c47
{konno.aoi} <{konno.aoi@justassist.co.jp}>
2024-02-14 16:04:42 +0900

    comitto

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";																																										
 import { getMessaging, getToken, onMessage } from "firebase/messaging";																																										
 																																										
-const firebaseConfig = {																																										
-    apiKey: "AIzaSyDsbox3yocR2jPj5AsLT6hbQpEl5bp3MZE",
-    authDomain: "pwa-53229.firebaseapp.com",
-    projectId: "pwa-53229",
-    storageBucket: "pwa-53229.appspot.com",
-    messagingSenderId: "740067812168",
-    appId: "1:740067812168:web:75a8d0cfbe0b3d19f6897d",
-    measurementId: "G-50YPV1RDRE"																																										
-};																																										
+const firebaseConfig = {
+  apiKey: "AIzaSyDsbox3yocR2jPj5AsLT6hbQpEl5bp3MZE",
+  authDomain: "pwa-53229.firebaseapp.com",
+  projectId: "pwa-53229",
+  storageBucket: "pwa-53229.appspot.com",
+  messagingSenderId: "740067812168",
+  appId: "1:740067812168:web:75a8d0cfbe0b3d19f6897d",
+  measurementId: "G-50YPV1RDRE"
+};																																									
 																																										
 initializeApp(firebaseConfig);																																										
 const messaging = getMessaging();	
