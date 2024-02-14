f3e75fd767a87cd86894f8a62ffca891dc43a4ea
{konno.aoi} <{konno.aoi@justassist.co.jp}>
2024-02-14 12:59:49 +0900

    cmitto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Notification from "./components/Notification";
+
 function App() {
   return (
     <div className="App">
