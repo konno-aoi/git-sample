3d2aef392068f15799670496b187b248e750cb1e
{konno.aoi} <{konno.aoi@justassist.co.jp}>
2024-02-15 09:51:18 +0900

    cmt

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";																																										
-import { getMessaging, getToken, onMessage } from "firebase/messaging";																																										
+import { getMessaging, getToken, onMessage } from "firebase/messaging";		
+import axios from 'axios';																																								
 																																										
 const firebaseConfig = {
   apiKey: "AIzaSyDsbox3yocR2jPj5AsLT6hbQpEl5bp3MZE",
